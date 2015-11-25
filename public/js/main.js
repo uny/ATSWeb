3558981ed06ef53d41c7daec69115c8e2ebfa27e
Yuki Nagai <ynagai@r.recruit.co.jp>
2015-11-25 14:13:53 +0900

    IMG

--- public/js/main.js
+++ public/js/main.js
@@ -24,4 +24,13 @@
     xhr.open('GET', AJAX_URLS[index], true);
     xhr.send();
   });
+
+  // IMG
+  var IMG_ENDPOINT = 'img/swift.png';
+  var IMG_SRCS = [`https://${BASE_URL}/${IMG_ENDPOINT}`, `http://${BASE_URL}/${IMG_ENDPOINT}`];
+  var IMG_NODE_IDS = ['https-img', 'http-img'];
+  IMG_NODE_IDS.forEach(function(element, index) {
+    var node = document.getElementById(element).getElementsByTagName('td')[1];
+    node.innerHTML = `<img alt="Swift" src="${IMG_SRCS[index]}">`
+  });
 })();
